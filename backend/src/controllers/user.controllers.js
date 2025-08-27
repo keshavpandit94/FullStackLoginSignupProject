50e83f363ac26c2dc4a32384ab962ca13a29dfbf
Keshav Chandra Pandit <134667288+keshavpandit94@users.noreply.github.com>
2025-08-27 19:48:35 +0530

    Update user.controllers.js

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -105,7 +105,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
-    sameSite: "Lax",
+    sameSite: "None",
   };
 
   return res
@@ -149,7 +149,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
-    sameSite: "Lax",
+    sameSite: "None",
   };
 
   return res
